89dd14b01a42e6717803ef359701c2526fbb347f
Eka Iqbal Virgiawan <ekaiqbalvirgiawan@gmail.com>
2021-07-22 23:10:21 +0700

    Using constants, remove dummyy usage, handle error auth

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,15 +1,15 @@
 import { useState, useEffect } from "react";
 import queryString from "query-string";
 import axios from "axios";
+import { API_BASE_URL } from "../../constants/spotify";
 import Navbar from "../../components/Navbar";
 import TrackCard from "../../components/TrackCard";
-import data from "../../data/dummy";
 import "./style.css";
 
 const Home = () => {
-  const [token, setToken] = useState();
-  const [searchQuery, setSearchQuery] = useState();
-  const [tracks, setTracks] = useState();
+  const [token, setToken] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+  const [tracks, setTracks] = useState([]);
 
   useEffect(() => {
     if (window.location.hash) {
@@ -22,7 +22,7 @@ const Home = () => {
   const handleSearch = (query) => {
     if (query) {
       axios
-        .get("https://api.spotify.com/v1/search", {
+        .get(`${API_BASE_URL}/search`, {
           headers: {
             Authorization: "Bearer " + token,
           },
@@ -32,7 +32,10 @@ const Home = () => {
             limit: 12,
           },
         })
-        .then((response) => setTracks(response.data.tracks.items));
+        .then((response) => setTracks(response.data.tracks.items))
+        .catch((error) => {
+          if (error.response.status === 401) alert("You should log in first!");
+        });
     }
   };
 
@@ -40,29 +43,27 @@ const Home = () => {
     <div className="page-container">
       <Navbar isLoggedIn={token ? true : false} />
       <h1 className="section-title">Playlists</h1>
-      <input className="search-input" onChange={(e) => setSearchQuery(e.target.value)} />
-      <button className="seatch-button" onClick={() => handleSearch(searchQuery)}>Search</button>
-      <h2 className="sub-section-title">
-        {tracks ? "Results" : "Suggestions"}
-      </h2>
+      <input
+        className="search-input"
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+      <button
+        className="seatch-button"
+        onClick={() => handleSearch(searchQuery)}
+      >
+        Search
+      </button>
+      <h2 className="sub-section-title">{tracks.length > 0 && "Results"}</h2>
       <div className="playlist-container">
-        {tracks
-          ? tracks.map((track) => (
-              <TrackCard
-                key={track.id}
-                trackName={track.name}
-                album={track.album}
-                artists={track.artists}
-              />
-            ))
-          : data.map((track) => (
-              <TrackCard
-                key={track.id}
-                trackName={track.name}
-                album={track.album}
-                artists={track.artists}
-              />
-            ))}
+        {tracks.length > 0 &&
+          tracks.map((track) => (
+            <TrackCard
+              key={track.id}
+              trackName={track.name}
+              album={track.album}
+              artists={track.artists}
+            />
+          ))}
       </div>
     </div>
   );
