99a36c54892da77d388cce745dac583bda73893f
Eka Iqbal Virgiawan <ekaiqbalvirgiawan@gmail.com>
2021-07-27 19:08:49 +0700

    Move the access token state to redux store

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import Home from './pages/home';
-import './App.css';
+import { Provider } from "react-redux";
+import store from "redux/stores";
+import Home from "./pages/home";
+import "./App.css";
 
 const App = () => {
   return (
-    <div className="app">
-      <Home />
-    </div>
+    <Provider store={store}>
+      <div className="app">
+        <Home />
+      </div>
+    </Provider>
   );
 };
 
