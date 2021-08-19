d74833e1cc283523218ac99fa4a480a05867d85e
Eka Iqbal Virgiawan <ekaiqbalvirgiawan@gmail.com>
2021-08-19 23:22:25 +0700

    Add and setup redux-persist

--- src/redux/stores/index.ts
+++ src/redux/stores/index.ts
@@ -1,13 +1,32 @@
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import userReducer from 'redux/reducers/user';
 
-const store = configureStore({
+const userPersistConfig = {
+  key: 'user',
+  storage,
+};
+
+export const store = configureStore({
   reducer: {
-    user: userReducer,
+    user: persistReducer(userPersistConfig, userReducer),
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
 });
-
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
-
-export default store;
+export const persistor = persistStore(store);
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
